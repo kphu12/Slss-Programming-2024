517020e7c96cefea71a616d770c722942b1e8cbf
Kendrick Phu <kphu1828@gmail.com>
2024-04-05 14:23:29 -0700

    Added to more function notes

---
+++ notes-2-2-morefunctions.py
@@ -0,0 +1,34 @@
+# More Functions
+# Author: Kendrick
+# 4 April  2024
+
+
+def stars(num_stars: int) -> str:
+    """Returns a given number of *"""
+    value = ""
+    
+    # if value == 0 or value == 1 "*"
+    # if value > 1 "*" * num_stars
+    # else negative number -> error
+    if num_stars == 0 or num_stars == 1:
+        value = "*"
+    elif num_stars> 1:
+        value = "*" * num_stars
+    else:
+        value = "Sorry, cant take negative numbers."
+
+    return value
+
+    return "*" * num_stars
+
+
+# Multiply Strings
+# greeting = "hello"
+# print(greeting * 2)
+
+# print("The quick brown fox jumps over the lazy dog." * 2)
+
+print(stars(1)) # *
+print(stars(100)) # "*" * 
+print(stars(0))
+print(stars(-1))
