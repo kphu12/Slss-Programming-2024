1cefc80509b2df629aae811e78cf268813fc2518
Kendrick Phu <kphu1828@gmail.com>
2024-04-16 09:48:16 -0700

    Added Pikachu subclass

--- notes-2-5-classes_objects.py
+++ notes-2-5-classes_objects.py
@@ -42,6 +42,11 @@ class Pokemon: # use a capital letter for class name
         else:
             return f"{self.name} batted the {food} away"
 
+# Create a new child class of Pokemon
+class Pikachu(Pokemon):
+    def __init__(self):
+        # Call constructor of parent class
+        super().__init__()
 
 # Create two Pokemon using our class
 # Make one Pokemon that is Pikachu
