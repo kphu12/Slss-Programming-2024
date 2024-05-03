f2961736d824a313598de1aee3d4eb20c74cb834
Kendrick Phu <kphu1828@gmail.com>
2024-05-03 09:18:54 -0700

    Added Mario!

---
+++ pygame-example-jewelthief.py
@@ -0,0 +1,83 @@
+import pygame as pg
+
+# --CONSTANTS--
+# COLOURS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+EMERALD = (21, 219, 147)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+
+WIDTH = 1280  # Pixels
+HEIGHT = 720
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        
+        self.image = pg.image.load("./Images/Mario.webp")
+    
+        self.rect = self.image.get_rect()
+    
+    def update(self):
+        """Updates the loaction of sprite to the mouse cursor"""
+        self.rect.centerx = pg.mouse.get_pos()[0]
+        self.rect.centery = pg.mouse.get_pos()[0]
+
+
+def start():
+    """Environment Setup and Game Loop"""
+
+    pg.init
+    pg.mouse.set_visible(False)
+
+    pg.init()
+
+    # --Game State Variables--
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    done = False
+    clock = pg.time.Clock()
+
+    # All sprites go in this sprite Group
+    all_sprites = pg.sprite.Group()
+
+    # Create a player and store it in a varialbe
+    player = Player()
+
+    all_sprites.add(player)
+
+    pg.display.set_caption("Jewel Thief Clone")
+
+    # --Main Loop--
+    while not done:
+        # --- Event Listener
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+
+        # --- Update the world state
+        all_sprites.update()
+
+        # --- Draw items
+        screen.fill(BLACK)
+
+        all_sprites.draw(screen)
+
+        # Update the screen with anything new
+        pg.display.flip()
+
+        # --- Tick the Clock
+        clock.tick(60)  # 60 fps
+
+
+def main():
+    start()
+
+
+if __name__ == "__main__":
+    main()
