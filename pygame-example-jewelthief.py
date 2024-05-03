74686b3932c17cc68e306dc48dde7d572c8c6473
Kendrick Phu <kphu1828@gmail.com>
2024-05-03 09:52:47 -0700

    Added Collision to mario

--- pygame-example-jewelthief.py
+++ pygame-example-jewelthief.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 # --CONSTANTS--
 # COLOURS
@@ -14,7 +15,7 @@ WIDTH = 1280  # Pixels
 HEIGHT = 720
 SCREEN_SIZE = (WIDTH, HEIGHT)
 
-
+NUM_COINS = 1
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
@@ -27,16 +28,28 @@ class Player(pg.sprite.Sprite):
     def update(self):
         """Updates the loaction of sprite to the mouse cursor"""
         self.rect.centerx = pg.mouse.get_pos()[0]
-        self.rect.centery = pg.mouse.get_pos()[0]
+        self.rect.centery = pg.mouse.get_pos()[1]
 
+class Coin(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self. image = pg.image.load("./Images/Coin.png")
+        
+        self.rect = self.image.get_rect()
+
+        self.rect.centerx = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.centery = random.randrange(0, HEIGHT - self.rect.height)
+
+        
+    
 
 def start():
     """Environment Setup and Game Loop"""
 
-    pg.init
+    pg.init()
     pg.mouse.set_visible(False)
 
-    pg.init()
 
     # --Game State Variables--
     screen = pg.display.set_mode(SCREEN_SIZE)
@@ -45,6 +58,15 @@ def start():
 
     # All sprites go in this sprite Group
     all_sprites = pg.sprite.Group()
+
+    # Coin Sprities
+    coin_sprites = pg.sprite.Group()
+
+    for _ in range(NUM_COINS):
+        coin = Coin()
+
+        all_sprites.add(coin)
+        coin_sprites.add(coin)
 
     # Create a player and store it in a varialbe
     player = Player()
@@ -63,8 +85,21 @@ def start():
         # --- Update the world state
         all_sprites.update()
 
+        # Coillision between player and coin_sprites
+        # Get a list of ALL coin_sprites that collide
+        #      with a player
+        # FOr every coin, that colides, print "COLLISION!"
+        coins_collided = pg.sprite.spritecollide(
+            player,
+            coin_sprites,
+            False
+        )
+
+        for coin in coins_collided:
+            print(f"COLLISION at {coin.rect.x}, {coin.rect.y}")
+
         # --- Draw items
-        screen.fill(BLACK)
+        screen.fill(WHITE)
 
         all_sprites.draw(screen)
 
